bfe28b4041682cb8299d2df07f6bf643cee5c4fa
Ezequiel Diaz <ezequiel.diaz1337@gmail.com>
2025-06-08 01:34:12 -0300

    Elimina la importación del componente AboutSection en PortfolioClient, simplificando el código y mejorando la legibilidad del archivo.

--- app/PortfolioClient.tsx
+++ app/PortfolioClient.tsx
@@ -24,7 +24,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
 import SkillsSection from "@/components/SkillsSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import SidebarIndex from "@/components/SidebarIndex";
