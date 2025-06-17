e4dca2a3a3bbd238f9c778ee94919b2bfd6d741b
Ezequiel Diaz <ezequiel.diaz1337@gmail.com>
2025-06-17 17:07:30 -0300

    Actualiza las palabras clave en los metadatos del portafolio para incluir términos adicionales relacionados con la ubicación y el rol, mejorando la optimización para motores de búsqueda.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   description:
     "Portfolio de Ezequiel Diaz, Full Stack Developer con 4+ años de experiencia en React, Next.js, TypeScript y Node.js. Especializado en e-commerce y aplicaciones SaaS.",
   keywords:
-    "Ezequiel Diaz, Full Stack Developer, React Developer, Next.js, TypeScript, Node.js, Portfolio, Full-Stack Developer, Back-end Developer, Full Stack Developer, React Developer, Next.js, TypeScript, Node.js, Portfolio, Córdoba Argentina",
+    "Ezequiel Diaz, Full Stack Developer, React Developer, Next.js, TypeScript, Node.js, Portfolio, Full-Stack Developer, Back-end Developer, Full Stack Developer, React Developer, Next.js, TypeScript, Node.js, Portfolio, Córdoba Argentina, Córdoba, Programador Córdoba, Desarrollador Ssr Córdoba",
   openGraph: {
     title: "Ezequiel Diaz - Full-Stack Developer",
     description: "Descubre mis proyectos y experiencia como Full Stack Developer",
